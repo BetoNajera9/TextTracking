c0f123e3048db9d2d472b0c5216686a372024448
Beto Najera <ing.betonajera@gmail.com>
2022-05-30 12:59:09 -0500

    :package: build: Add mdi and formkit packages
    Add mdi and formkit packages

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { plugin, defaultConfig } from '@formkit/vue'
+import mdiVue from 'mdi-vue/v3'
+import * as mdijs from '@mdi/js'
+import '@formkit/themes/genesis'
 
-createApp(App).mount('#app')
+createApp(App)
+	.use(mdiVue, {
+		icons: mdijs,
+	})
+	.use(plugin, defaultConfig)
+	.mount('#app')

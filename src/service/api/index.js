1b32123dc562f1b2994c82731fdd005a2f7754b4
Beto Najera <ing.betonajera@gmail.com>
2022-06-29 00:21:39 -0500

    :sparkles: feat: Add setCustommer to api service
    Add setCustomer to api service

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -17,8 +17,17 @@ export default class {
 
 	async getCustomers() {
 		const response = await fetch('/api/customers', this.getConfig)
-		const data = await response.json()
+		const { data } = await response.json()
 		return data
+	}
+
+	async setCustomer(data) {
+		const response = await fetch('/api/customer', {
+			...this.requestOptions,
+			body: JSON.stringify(data),
+		})
+		const res = await response.json()
+		return res.data
 	}
 
 	async getStock() {
@@ -33,15 +42,15 @@ export default class {
 		return data
 	}
 
-	async addClient(client) {
-		const raw = JSON.stringify(client)
+	async addCustomers(customer) {
+		const raw = JSON.stringify(customer)
 		this.getConfig.body = raw
 
 		await fetch('localhost:3000/api/customer', this.getConfig)
 	}
 
-	async addSale(client) {
-		const raw = JSON.stringify(client)
+	async addSale(customer) {
+		const raw = JSON.stringify(customer)
 		this.getConfig.body = raw
 
 		await fetch('localhost:3000/api/customer', this.getConfig)

798ef1764e45afac6078c6cc1c0581e8cf17e236
BetoNajera <ing.betonajera@gmail.com>
2022-07-09 22:23:51 -0500

    :recycle: refactor: Remove deprecated files
    Remove deprecated files

--- api/config/envServer.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-
-export const api = {
-	name: process.env.APP_NAME || '',
-	server: process.env.SERVER_URL || '',
-	env: process.env.NODE_ENV || 'development',
-	port: process.env.PORT || '3000',
-	cors: process.env.CORS || '',
-}
--- src/store/modules/index.js
+++
@@ -1,4 +0,0 @@
-export * from './customers.module'
-export * from './stock.module'
-export * from './history.module'
-export * from './accountStatements.module'

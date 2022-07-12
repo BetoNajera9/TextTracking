67bb3ad5b19cbdcd808250ccfe0f983541a5fe86
BetoNajera <ing.betonajera@gmail.com>
2022-07-12 16:18:14 -0500

    :zap: perf: remove console.log line
    Remove console.log line

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -63,7 +63,6 @@ export default {
 		}
 
 		watch(searchInput, (newSearchInput) => {
-			console.log(props.propSearch)
 			if (props.propSearch !== 'id')
 				searchInput.value = newSearchInput.toUpperCase()
 		})

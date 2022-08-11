0bcd56f5705fae8504f0bb11aefd1d6aeb8a65e9
betonajera9 <ing.betonajera@gmail.com>
2022-08-11 00:33:04 -0500

    :zap: perf: Add Api foler on service folder
    Add Api folder on service folder

---
+++ electron/backend/service/api/index.ts
@@ -0,0 +1,4 @@
+export { accountStatement as setAccountStatement } from './accountStatement'
+export { Customer as setCustomers } from './customers'
+export { Sale as setSales } from './sales'
+export { Stock as setStock } from './stock'

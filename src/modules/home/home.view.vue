8edf9b4371b2f860160c38cf3098e89f3dfeb3a5
BetoNajera <ing.betonajera@gmail.com>
2024-06-11 12:05:44 -0600

    :sparkles: Feat: Create home module
    Create home module

---
+++ src/modules/home/home.view.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="title">
+		<h1 class="text-red">TextTrack</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'home',
+	setup() {
+		return {}
+	},
+}
+</script>
+
+<style scoped>
+.title {
+	display: flex;
+	justify-content: center;
+}
+</style>

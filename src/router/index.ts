be71cfff507e9e839535548562bc11b8d65e570f
BetoNajera <ing.betonajera@gmail.com>
2024-06-11 12:04:57 -0600

    :sparkles: Feat: Create vue router
    Create vue router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,54 +1,60 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AccountStatesContent from '../views/AccountStatesContent.vue'
-import CustomerContent from '../views/CustomerContent.vue'
-import HistoryContent from '../views/HistoryContent.vue'
-import SalesContent from '../views/SalesContent.vue'
-import StockContent from '../views/StockContent.vue'
-import DebitBalance from '../views/DebitBalance.vue'
-import Home from '../views/Home.vue'
 
+import AccountStates from '@account-statement/account-statement.view.vue'
+import PendingPayments from '@pending-payment/pending-payment.view.vue'
+import Customer from '@customer/customer.view.vue'
+import Storage from '@storage/storage.view.vue'
+import History from '@history/history.view.vue'
+import Returns from '@returns/returns.view.vue'
+import Sales from '@sales/sales.view.vue'
+import Home from '@home/home.view.vue'
 
 const routes = [
-	{
-		path: '/',
-		name: 'home',
-		component: Home,
-	},
-	{
-		path: '/customer',
-		name: 'customer',
-		component: CustomerContent,
-	},
-	{
-		path: '/sales',
-		name: 'sales',
-		component: SalesContent,
-	},
-	{
-		path: '/stock',
-		name: 'stock',
-		component: StockContent,
-	},
-	{
-		path: '/history',
-		name: 'history',
-		component: HistoryContent,
-	},
-	{
-		path: '/account-states',
-		name: 'account-states',
-		component: AccountStatesContent,
-	},
-	{
-		path: '/debit-balance',
-		name: 'debit-balance',
-		component: DebitBalance,
-	},
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/customer',
+    name: 'customer',
+    component: Customer,
+  },
+  {
+    path: '/sales',
+    name: 'sales',
+    component: Sales,
+  },
+  {
+    path: '/storage',
+    name: 'storage',
+    component: Storage,
+  },
+  {
+    path: '/history',
+    name: 'history',
+    component: History,
+  },
+  {
+    path: '/account-states',
+    name: 'account-states',
+    component: AccountStates,
+  },
+  {
+    path: '/pending-payments',
+    name: 'pending-payments',
+    component: PendingPayments,
+  },
+  {
+    path: '/returns',
+    name: 'returns',
+    component: Returns,
+  },
 ]
 
 const router = createRouter({
-	history: createWebHistory(process.env.BASE_URL),
-	routes,
+  history: createWebHistory(import.meta.url),
+  routes,
 })
 
 export default router
